f674f5b89c1e33665534bb7ea2dab2a5df6b357b
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-08-01 12:32:19 -0500

    completed HelloWorld program

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+// HelloWorld Program:
+// Define the class and ensure the name of the class matches the name of the src file
+public class HelloWorld {
+    // Define the main method and entry point to the application
+    public static void main(String[] args) {
+        // Use System.out.println to outout info with a new line
+        System.out.println("Hello, World!");
+        // Use System.out.print to output info without a new line
+        System.out.print("Hello, ");
+        System.out.print("World!");
+    }
+}
