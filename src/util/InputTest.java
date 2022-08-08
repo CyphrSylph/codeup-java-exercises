adcfea7f2120cf012bb8b8421c4db69895a50c7b
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-08-08 16:59:27 -0500

    tested input class methods under main method

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,2 +1,14 @@
-package util;public class InputTest {
+package util;
+
+public class InputTest {
+    // Test all the methods from the Input class
+    public static void main(String[] arg) {
+        Input newInput = new Input();
+        newInput.getString();
+        newInput.getInt();
+        newInput.getDouble();
+        newInput.getInt(1,10);
+        newInput.getDouble(1,10);
+        newInput.yesNo();
+    }
 }
