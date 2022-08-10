004ca589c5d739f670ae0acf367456fd6809d115
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-08-10 16:46:19 -0500

    movies exercise completed

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,35 @@
-package movies;public class Movie {
+package movies;
+
+public class Movie {
+    // Private string fields
+    private String name;
+    private String category;
+
+    // Constructors
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return name + " -- " + category;
+    }
+
+    // Accessors
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
